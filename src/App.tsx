4bc4fba60b5e3a2a482aac2a6e483dd131e967bc
Sergio <sergiomendes23@gmail.com>
2022-12-12 20:10:49 -0300

    types components done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Container } from './/App.styles'
-
+import * as s from './/App.styles'
+import { useState } from 'react';
+import { item } from './types/item';
 
 const App = () => {
-  return (
-    <Container>
 
-    </Container>
+  const [list, setList] = useState<item[]>([
+    {id: 1, name: 'Fazer o hub', done: false},
+    {id: 2, name: 'Fazer o projetão', done: false}
+  ])
+
+  return (
+    <s.Container>
+      <s.Area>
+        <s.Header>Lista de Tarefas</s.Header>
+        {list.map((item, index) => (
+          <div>...</div>
+        ))}
+      </s.Area>
+    </s.Container>
   )
 }
 
