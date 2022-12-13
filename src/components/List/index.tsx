14e66ef45a29c8f4e03eea05ec75bcb6b0b66e84
Sergio <sergiomendes23@gmail.com>
2022-12-12 21:01:36 -0300

    Done component applicated

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,14 +1,22 @@
 import * as s from "./style";
-import { item } from "../../types/item"
+import { item } from "../../types/item";
+import { useState } from "react";
 
 type Props = {
     item: item
 }
 
 export const List = ({item}: Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.done);
+
     return (
-        <s.Container>
-            <input type="checkbox" />
+        <s.Container done={isChecked}>
+            <input 
+                type="checkbox" 
+                checked={isChecked}
+                onChange = {e => setIsChecked(e.target.checked)} 
+            />
             <label>{item.name}</label>
         </s.Container>
     )
