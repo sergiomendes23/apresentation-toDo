9ad3cff6d6e46e5d49040c2781cb3e721b73ab86
Sergio <sergiomendes23@gmail.com>
2022-12-13 14:23:34 -0300

    toDo done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 import { item } from './types/item';
-import { List } from './components/List';
-import { CreateList } from './components/CreateList';
+import { List } from './components/List/List';
+import { CreateList } from './components/CreateList/CreateList';
 
 const App = () => {
 
